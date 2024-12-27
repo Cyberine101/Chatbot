2d752e1fd17166c2fdd6cfc9ec87efaf133a93e2
Cyberine101 <bryce.a.green.bg@gmail.com>
2024-12-27 00:58:09 -0500

    Organized file directories and setup frontend and backend.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="hello"></input>
+    </div>
+  );
+}
+
+export default App;
